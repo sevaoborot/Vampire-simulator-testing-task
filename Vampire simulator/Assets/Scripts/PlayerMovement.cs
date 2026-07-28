0cee174d46b850b0caa9ba0071b431c01413a300
Усевалад Мартысюк | Usevalad Martysiuk <wsmartysuk@gmail.com>
2026-07-28 17:43:47 +0200

    Basic movement

---
+++ Vampire simulator/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour, VampSym_Actions.IPlayerActions
+{
+    //movement settings
+    [SerializeField] private float _movementSpeed;
+    private Vector2 _movementVector;
+
+    //input actions settings
+    private VampSym_Actions _actions;
+    private VampSym_Actions.PlayerActions _player;
+
+    private void Awake()
+    {
+        _actions = new VampSym_Actions();
+        _player = _actions.Player;
+        _player.AddCallbacks(this); //? чо ругается
+    }
+
+    private void Update()
+    {
+        Vector2 movementVector = _movementVector * _movementSpeed * Time.deltaTime;
+        transform.position = (Vector2)transform.position + movementVector;
+    }
+
+    private void OnEnable()
+    {
+        _player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _player.Disable();
+    }
+
+    private void OnDestroy()
+    {
+        _actions.Dispose();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _movementVector = context.ReadValue<Vector2>();
+    }
+}
